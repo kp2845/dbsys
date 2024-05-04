d75695ac52da94b98f527bf13cb85c1a73d0c608
Kairi Purnell <kp2845@nyu.edu>
2024-05-04 17:04:43 -0400

    DB Conn Cache change

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,8 @@
 <?php
-header("Cache-Control: no-cache");
+//header("Cache-Control: no-cache");
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
 $servername = "localhost";
 $username = "kairipur_kairidb";
 $password = "924Apple2!";
