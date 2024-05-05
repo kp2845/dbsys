b97ddcda06dd08591b73864532989b3f9fadc926
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 17:16:24 -0400

    Add managers CRUD table

--- managers.php
+++ managers.php
@@ -9,8 +9,8 @@ if($result->num_rows > 0) {
         echo "<td><td>" . $row["mgr_id"] . "</td><td>" . $row["budget"] . "</td><td>"
             . $row["direct_reports"] . "</td><td>" . $row["total_reports"] . "</td><td>" . $row["mentee"] . "</td><td>"
             . $row["departments_dept_id"] . "</td><td>" . $row["team_name"]
-            . "</td><td><a href='delmanagers.php?mgr_id=" . $row["mgr_id"] . "'>Delete Record</a>"
-            . "</td><td><a href='editmanagers.php?mgr_id=" . $row["mgr_id"] . "'>Edit Record</a>"
+            . "</td><td><a href='delmanagers.php?mgr_id=" . $row["team_name"] . "'>Delete Record</a>"
+            . "</td><td><a href='editmanagers.php?mgr_id=" . $row["team_name"] . "'>Edit Record</a>"
             . "</td></tr>";
     }
 }
