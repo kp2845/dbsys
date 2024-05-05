0b9a9327e84a9006eff79d616071e621c7b8fe33
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 18:32:12 -0400

    Add managers CRUD table

--- updatemanagers.php
+++ updatemanagers.php
@@ -1,10 +1,9 @@
 <?php
 include "dbconn.php";
 
-$sql = "update managers set mgr_id = ?, budget = ?, direct_reports = ?, total_reports = ?, mentee = ?, departments_dept_id = ?, team_name = ?,  where team_name = ?";
+$sql = "update managers set mgr_id = ?, budget = ?, direct_reports = ?, total_reports = ?, mentee = ?, departments_dept_id = ?, team_name = ?  where team_name = ?";
 
 //set parameters to form requst
-$team_name = $_REQUEST["team_name"];
 $mgr_id = $_REQUEST["mgr_id"];
 $budget = $_REQUEST["budget"];
 $direct_reports = $_REQUEST["direct_reports"];
@@ -12,10 +11,11 @@ $total_reports = $_REQUEST["total_reports"];
 $mentee = $_REQUEST["mentee"];
 $departments_dept_id = $_REQUEST["departments_dept_id"];
 $team_name = $_REQUEST["team_name"];
+$team_name = $_REQUEST["team_name"];
 
 //prepare and bind values
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sidiiss", $team_name, $mgr_id, $budget, $direct_reports, $total_reports, $mentee, $departments_dept_id);
+$stmt->bind_param("idiisss", $mgr_id, $budget, $direct_reports, $total_reports, $mentee, $departments_dept_id, $team_name);
 echo "<b>Adding New Records......Please wait</b><br>";
 if($stmt->execute() === TRUE) {
     echo "<script>window.location.href = 'managers.php'</script>";
