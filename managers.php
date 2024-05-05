30953ea811c45ee6d68f55de897d86e2f125f964
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 17:03:44 -0400

    Add managers CRUD table

--- managers.php
+++ managers.php
@@ -1,14 +1,14 @@
 <?php
 include "menu.php";
 include "dbconn.php";
-echo "<table border=1><border-color=#FE5302><tr><th>Manager ID</th><th>Budget</th><th>Direct Reports</th><th>Total Reports</th><th>Mentee</th><th>Department ID</th></tr>";
+echo "<table border=1><border-color=#FE5302><tr><th>Manager ID</th><th>Budget</th><th>Direct Reports</th><th>Total Reports</th><th>Mentee</th><th>Department ID</th><th>Team Name</th></tr>";
 $sql = "select * from managers";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<td><td>" . $row["mgr_id"] . "</td><td>" . $row["budget"] . "</td><td>"
             . $row["direct_reports"] . "</td><td>" . $row["total_reports"] . "</td><td>" . $row["mentee"] . "</td><td>"
-            . $row["dept_id"] . "</td><td>" . $row["team_name"] . "</td></tr>"
+            . $row["dept_id"] . "</td><td>" . $row["team_name"]
             . "</td><td><a href='delmanagers.php?mgr_id=" . $row["mgr_id"] . "'>Delete Record</a>"
             . "</td><td><a href='editmanagers.php?mgr_id=" . $row["mgr_id"] . "'>Edit Record</a>"
             . "</td></tr>";
