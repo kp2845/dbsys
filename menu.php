4d98c00e0edce9d735b5267124cffd5a60f3f0f7
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 16:58:16 -0400

    Add managers CRUD table

--- menu.php
+++ menu.php
@@ -14,7 +14,7 @@ echo <<<MENU
     <a href="education.php">Education</a></li>
     <a href="locations.php">Locations</a></li>
     <a href="departments.php">Departments</a></li>
-    <a href="managers.php">Departments</a></li>
+    <a href="managers.php">Managers</a></li>
     <br><br>
     <p><h3>This section contains all reports below</h3></p>
     <a href="reports1.php">Audit Report - Broken</a></li>
