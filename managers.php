c6293427c5dcd046e67f0fd1e7eb97e127c694c0
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 17:22:09 -0400

    Add managers CRUD table

--- managers.php
+++ managers.php
@@ -6,7 +6,7 @@ $sql = "select * from managers";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<td><td>" . $row["mgr_id"] . "</td><td>" . $row["budget"] . "</td><td>"
+        echo "<tr><td>" . $row["mgr_id"] . "</td><td>" . $row["budget"] . "</td><td>"
             . $row["direct_reports"] . "</td><td>" . $row["total_reports"] . "</td><td>" . $row["mentee"] . "</td><td>"
             . $row["departments_dept_id"] . "</td><td>" . $row["team_name"]
             . "</td><td><a href='delmanagers.php?mgr_id=" . $row["team_name"] . "'>Delete Record</a>"
