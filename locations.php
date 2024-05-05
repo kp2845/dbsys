cf1d59eff79b6fe3f92ae321a113d83325af5e91
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 12:41:06 -0400

    Adding Locations Table CRUD

--- locations.php
+++ locations.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["location_id"] . "</td><td>" . $row["location_type"] . "</td><td>"
-        . $row["location_status"] . "</td><td>" . $row["location_Tax"] . "</td><td>" . $row["location_capacity"]
+        . $row["location_status"] . "</td><td>" . $row["location_tax"] . "</td><td>" . $row["location_capacity"]
         . "</td><td><a href='dellocations.php?location_id=" . $row["location_id"] . "'>Delete Record</a>"
         . "</td><td><a href='editlocations.php?location_id=" . $row["location_id"] . "'>Edit Record</a>"
         . "</td></tr>";
