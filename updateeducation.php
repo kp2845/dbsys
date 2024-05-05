70645480277e1f0cec2adeb7c1a4c22982fc614f
Kairi Purnell <kp2845@nyu.edu>
2024-05-04 21:55:33 -0400

    Adding Education Table CRUD

--- updateeducation.php
+++ updateeducation.php
@@ -1,10 +1,10 @@
 <?php
 include "dbconn.php";
 
-$sql = "update Education set assoc_score = ?, leader_score = ?, univ_score = ? where education_id = ?";
+$sql = "update Education set Emp_ID = ?, assoc_score = ?, leader_score = ?, univ_score = ? where education_id = ?";
 
 //set parameters to form requst
-//$education_id = $_REQUEST["education_id"]; Not sure if this will be needed
+$Emp_ID = $_REQUEST["Emp_ID"]; //Not sure if this will be needed
 $assoc_score = $_REQUEST["assoc_score"];
 $leader_score = $_REQUEST["leader_score"];
 $univ_score = $_REQUEST["univ_score"];
@@ -12,7 +12,7 @@ $education_id = $_REQUEST["education_id"];
 
 //prepare and bind values
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("iiss", $assoc_score, $leader_score, $univ_score,  $education_id);
+$stmt->bind_param("iiiss", $Emp_ID, $assoc_score, $leader_score, $univ_score,  $education_id);
 echo "<b>Adding New Records......Please wait</b><br>";
 if($stmt->execute() === TRUE) {
     echo "<script>window.location.href = 'education.php'</script>";
