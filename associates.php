3207ef03b3f2d4bb558955828102b18b36914ca3
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 20:17:02 -0400

    Update Associates Table with 5 new columns

--- associates.php
+++ associates.php
@@ -1,7 +1,7 @@
 <?php
 include "menu.php";
 include "dbconn.php";
-echo "<table border=1><border-color=#FE5302><tr><th>First Name</th><th>Last Name</th><th>Salary</th><th>Bonus</th><th>Email</th><th>Mentor</th><th>Associate Street</th><th>Associate City</th><th>Associate State</th><th>Assoc_Zip</th></tr>";
+echo "<table border=1><border-color=#FE5302><tr><th>First Name</th><th>Last Name</th><th>Salary</th><th>Bonus</th><th>Email</th><th>Mentor</th><th>Associate Street</th><th>Associate City</th><th>Associate State</th><th>Associate Zip</th></tr>";
 $sql = "select * from Associates";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
@@ -9,7 +9,7 @@ if($result->num_rows > 0) {
         echo "<tr><td>" . $row["Fname"] . "</td><td>" . $row["Lname"] . "</td><td>" 
         . $row["Salary"] . "</td><td>" . $row["Bonus"] . "</td><td>" . $row["Email"] . "</td><td>"
             . $row["Mentor"] . "</td><td>" . $row["Assoc_Street"] . "</td><td>" . $row["Assoc_City"] . "</td><td>"
-            . $row["Assoc_State"] . "</td><td>" . $row["Assoc_zip"]
+            . $row["Assoc_State"] . "</td><td>" . $row["Assoc_Zip"]
         . "</td><td><a href='delassoc.php?EMP_ID=" . $row["EMP_ID"] . "'>Delete Record</a>"
         . "</td><td><a href='editassoc.php?EMP_ID=" . $row["EMP_ID"] . "'>Edit Record</a>" 
         . "</td></tr>";
