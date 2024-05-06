331e91b7a3d3df39faf5b3d7925839139de6625d
Kairi Purnell <kp2845@nyu.edu>
2024-05-06 01:38:02 -0400

    Audits

--- assocaudit.php
+++ assocaudit.php
@@ -7,7 +7,7 @@ $result = $conn->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["id"] . "</td><td>" . $row["old_first_name"] . "</td><td>"
-            . $row["old_last_name"] . "</td><td>" . $row["old_email"]  . "</td></tr>";"
+            . $row["old_last_name"] . "</td><td>" . $row["old_email"]  . "</td></tr>";
     }
 }
 echo "</table>";
