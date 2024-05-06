fc28b96932a4f6c0f3dcb4040a5e8bff5dbccecf
Kairi Purnell <kp2845@nyu.edu>
2024-05-05 20:16:16 -0400

    Update Associates Table with 5 new columns

--- associates.php
+++ associates.php
@@ -1,7 +1,7 @@
 <?php
 include "menu.php";
 include "dbconn.php";
-echo "<table border=1><border-color=#FE5302><tr><th>First Name</th><th>Last Name</th><th>Salary</th><th>Bonus</th><th>Email</th><th>Mentor</th><th>Assoc_Street</th><th>Assoc_City</th><th>Assoc_State</th><th>Assoc_Zip</th></tr>";
+echo "<table border=1><border-color=#FE5302><tr><th>First Name</th><th>Last Name</th><th>Salary</th><th>Bonus</th><th>Email</th><th>Mentor</th><th>Associate Street</th><th>Associate City</th><th>Associate State</th><th>Assoc_Zip</th></tr>";
 $sql = "select * from Associates";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
@@ -10,7 +10,7 @@ if($result->num_rows > 0) {
         . $row["Salary"] . "</td><td>" . $row["Bonus"] . "</td><td>" . $row["Email"] . "</td><td>"
             . $row["Mentor"] . "</td><td>" . $row["Assoc_Street"] . "</td><td>" . $row["Assoc_City"] . "</td><td>"
             . $row["Assoc_State"] . "</td><td>" . $row["Assoc_zip"]
-        . "</td><td><a href='delassoc.php?EMP_ID=" . $row["EMP_ID"] . "'>Deletes Record</a>"
+        . "</td><td><a href='delassoc.php?EMP_ID=" . $row["EMP_ID"] . "'>Delete Record</a>"
         . "</td><td><a href='editassoc.php?EMP_ID=" . $row["EMP_ID"] . "'>Edit Record</a>" 
         . "</td></tr>";
     }
