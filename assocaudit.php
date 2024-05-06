9454ab485508a577ce246cb3a98586e564786c0c
Kairi Purnell <kp2845@nyu.edu>
2024-05-06 01:42:13 -0400

    Audits

--- assocaudit.php
+++ assocaudit.php
@@ -2,7 +2,7 @@
 include "menu.php";
 include "dbconn.php";
 echo "<p><h3>You are viewing the Audit Report</h3></p>";
-echo "<table border=1><border-color=#FE5302><tr><th>Old id</th><th>Old FName</th><th>Old LName</th><th>Old Email</th></tr>";
+echo "<table border=1><border-color=#FE5302><tr><th>Old id</th><th>Old First Name</th><th>Old Last Name</th><th>Old Email</th></tr>";
 $sql = "select * from Associates_Audit";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
